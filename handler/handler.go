9197d13131d66e57f1bc97dd10fabd9459bbfc57
milospp <misa01523@gmail.com>
2023-06-29 10:25:36 +0200

    feat: Added rest endpoint for addiing and reading notifications

--- handler/handler.go
+++ handler/handler.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/opentracing/opentracing-go"
 	"github.com/windbnb/notification-service/model"
 	"github.com/windbnb/notification-service/service"
@@ -43,29 +45,77 @@ func (handler *Handler) PutNotificationSettings(w http.ResponseWriter, r *http.R
 	json.NewEncoder(w).Encode(notificationSettings)
 }
 
-// func (handler *Handler) GetHostAverageReview(w http.ResponseWriter, r *http.Request) {
-// 	span := tracer.StartSpanFromRequest("hostRatingHandler", handler.Tracer, r)
-// 	defer span.Finish()
-// 	span.LogFields(
-// 		tracer.LogString("handler", fmt.Sprintf("handling rating host at %s\n", r.URL.Path)),
-// 	)
+func (handler *Handler) GetNotificationSettings(w http.ResponseWriter, r *http.Request) {
+	span := tracer.StartSpanFromRequest("getNotificationSettingsHandler", handler.Tracer, r)
+	defer span.Finish()
+	span.LogFields(
+		tracer.LogString("handler", fmt.Sprintf("handling rating host at %s\n", r.URL.Path)),
+	)
 
-// 	params := mux.Vars(r)
-// 	hostId, _ := strconv.ParseUint(params["id"], 10, 32)
+	params := mux.Vars(r)
+	hostId, _ := strconv.ParseUint(params["id"], 10, 32)
 
-// 	ctx := tracer.ContextWithSpan(context.Background(), span)
-// 	hostRating, err := handler.Service.GetAverageHostRating(uint(hostId), ctx)
+	ctx := tracer.ContextWithSpan(context.Background(), span)
+	hostRating, err := handler.Service.GetUserNotificationSettings(uint(hostId), ctx)
 
-// 	w.Header().Set("Content-Type", "application/json")
-// 	if err != nil {
-// 		tracer.LogError(span, err)
-// 		w.WriteHeader(http.StatusUnauthorized)
-// 		json.NewEncoder(w).Encode(model.ErrorResponse{Message: err.Error(), StatusCode: http.StatusUnauthorized})
-// 		return
-// 	}
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		tracer.LogError(span, err)
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Message: err.Error(), StatusCode: http.StatusUnauthorized})
+		return
+	}
 
-// 	json.NewEncoder(w).Encode(hostRating)
-// }
+	json.NewEncoder(w).Encode(hostRating)
+}
+
+func (handler *Handler) SaveNotification(w http.ResponseWriter, r *http.Request) {
+	span := tracer.StartSpanFromRequest("saveNotificationHandler", handler.Tracer, r)
+	defer span.Finish()
+	span.LogFields(
+		tracer.LogString("handler", fmt.Sprintf("handling saving notification at %s\n", r.URL.Path)),
+	)
+
+	var notificationRequest model.NotificationRequest
+	json.NewDecoder(r.Body).Decode(&notificationRequest)
+
+	ctx := tracer.ContextWithSpan(context.Background(), span)
+	notification, err := handler.Service.SaveNotification(&notificationRequest, ctx)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		tracer.LogError(span, err)
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Message: err.Error(), StatusCode: http.StatusUnauthorized})
+		return
+	}
+
+	json.NewEncoder(w).Encode(notification)
+}
+
+func (handler *Handler) GetUserRecentNotification(w http.ResponseWriter, r *http.Request) {
+	span := tracer.StartSpanFromRequest("getUserRecentNotificationHandler", handler.Tracer, r)
+	defer span.Finish()
+	span.LogFields(
+		tracer.LogString("handler", fmt.Sprintf("handling reading user notification at %s\n", r.URL.Path)),
+	)
+
+	params := mux.Vars(r)
+	userId, _ := strconv.ParseUint(params["id"], 10, 32)
+
+	ctx := tracer.ContextWithSpan(context.Background(), span)
+	notifications, err := handler.Service.GetUserRecentNotification(uint(userId), ctx)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		tracer.LogError(span, err)
+		w.WriteHeader(http.StatusUnauthorized)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Message: err.Error(), StatusCode: http.StatusUnauthorized})
+		return
+	}
+
+	json.NewEncoder(w).Encode(notifications)
+}
 
 // func (handler *Handler) RateAccomodation(w http.ResponseWriter, r *http.Request) {
 // 	span := tracer.StartSpanFromRequest("accomodationRatingHandler", handler.Tracer, r)
--- model/model.go
+++ model/model.go
@@ -1,10 +1,15 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Notification struct {
 	ID         primitive.ObjectID `bson:"_id"`
 	UserId     uint               `bson:"userId"`
+	Timestamp  time.Time          `bson:"timestamp"`
 	NotifTitle string             `bson:"notifTitle"`
 	NotifType  string             `bson:"notifType"`
 	Seen       bool               `bson:"seen"`
@@ -15,7 +20,7 @@ type NotificationSettings struct {
 	UserId              uint               `bson:"userId"`
 	ResRequest          bool               `bson:"resRequest"`
 	ResCancel           bool               `bson:"resCancel"`
-	HostReview          bool               `bson:"hostReview"`
+	HostReview          bool               `bson:"HostReview"`
 	AccommodationReview bool               `bson:"accommodationReview"`
-	ReservationResponse bool               `bson:"reservationResponse"`
+	ReservationResponse bool               `bson:"rservationResponse"`
 }
